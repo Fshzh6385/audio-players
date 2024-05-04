35d5fa86c5ef353f402d10910846501acc7367cb
Fshzh6385 <165472139+Fshzh6385@users.noreply.github.com>
2024-05-04 19:50:37 +0330

    add audioplayers

---
+++ js/script.js
@@ -0,0 +1,388 @@
+let audioFiles = [
+    { 
+        src: 'sounds/track1.mp3',
+        img: 'img/img1.jpg', 
+        title: '  سردار من  ', 
+        lyrics: `تویی برگشتی و میگی که حرفت تنها جنگت نیست
+        نشون غیرتت تنها همین تیر و تفنگت نیست
+        تویی برگشتی و انگار میخوای فانوس و برداری
+        تو خوب از نقشه ها از معبرا از ما خبر داری
+        بگو سردار من وقتی گلوله چاره گر میشه
+        که تو قلبای این مردم امید و عدل جاری شه
+        بگو سردار من خرازیه بیدار میدون ها
+        مگه میشه فقط اسمت بمونه رو خیابون ها
+        بگو سردار من دلدار من فرمانده ی بیدار من
+        از دل بگو از جان بگو لب تر کن ای طوفان
+        بگو از دیدنی هایت بگو از رسم و از راهت
+        بگو روشن بگو راهت بگو از قبله از ماهت
+        بگو بگو بگو بگو  بپرس از هم قطارا کو دعا هایی که میکردی
+        قرار ما نبود از راهی که رفتیم برگردیم
+        قرار این بود مثل کاظمی روزا و شب هامون
+        فقط اسم حسین باشه تو دل هامون رو لب هامون
+        بگو سردار من از عشق از اشکای تو خلوت
+        بگو از خادم سرباز ها بودن میون خط
+        چه دستایی گرفتی تو با این دستی که افتاده
+        یه دست تو صدا داره که دل هارو تکون داده
+        دل عالم هوایی و دل عاشق هوایی تر
+        یه دنیا کربلایی ام حسینا کربلایی تر
+        بگو ای زنده روح زنده رود باید که از عشقت سرود
+        مجنون اگر افسانه بود اما تو در مجنون خودت مجنون شدی در خون
+        خودت مجنون شدی در خون
+        شکوه مرد ها پیداست ولی تو رزم پیدا تر
+        علمدارا همه زیبان ولی بی دست زیباتر
+        همیشه مرد ها مردن اگه سردار اگه بی سر
+        امیر قلب ها هستن چه تو زاغه چه تو سنگر
+        اگه سردار اگه بی سر زیباتر
+        بگو سردار من دلدار من فرمانده ی بیدار من
+        از دل بگو از جان بگو لب تر کن ای طوفان
+        بگو از دیدنی هایت بگو از رسم و از راهت
+        بگو روشن بگو راهت بگو از قبله از ماهت
+        بگو بگو بگو بگو`
+    },
+    { 
+        src: 'sounds/track2.mp3', 
+        img: 'img/img2.jpg', 
+        title: '  سلام فرمانده ', 
+        lyrics: `  یا مولا یا صاحب الزمان
+        الغوث الغوث الغوث ادرکنی
+        ⊶⊰✾⊱⊷⊶⊰✾⊱⊷⊶⊰✾⊱⊷
+        عشق جانم امام زمانم
+        عشق جانم امام زمانم
+        عشق جانم امام زمانم
+        عشق جانم
+        ⊶⊰✾⊱⊷⊶⊰✾⊱⊷⊶⊰✾⊱⊷
+        دنیا بدون تو معنایی نداره
+        عشق روزگارم وقتی که تو باشی دنیامون بهاره
+        دنیای بدون تو معنایی نداره
+        عشق روزگارم وقتی که تو باشی دنیامون بهاره
+        
+        ⊶⊰✾⊱⊷⊶⊰✾⊱⊷⊶⊰✾⊱⊷
+        
+        سلام‌فرمانده سلام از این نسل غیور جامانده
+        سـلام فرمانده سیدعلی دهه‌ی نودی‌ هاشو فراخوانده
+        سلام فرمانده
+        
+        ⊶⊰✾⊱⊷⊶⊰✾⊱⊷⊶⊰✾⊱⊷
+        
+        بیا جون من بیا ، بیا یارت میشم
+        هوادارت میشم گرفتارت میشم
+        علی بن مهزیارت میشم
+        با همین قد کوچیکم خودم سردارت میشم
+        
+        ⊶⊰✾⊱⊷⊶⊰✾⊱⊷⊶⊰✾⊱⊷
+        
+        بیا جون من بیا ، بیا یارت میشم
+        هوادارت میشم گرفتارت میشم
+        علی بن مهزیارت میشم
+        با همین قد کوچیکم خودم سردارت میشم
+        
+        ⊶⊰✾⊱⊷⊶⊰✾⊱⊷⊶⊰✾⊱⊷
+        
+        سلام فرمانده سلام از این نسل غیور جامانده
+        سـلام فرمانده سیدعلی دهه‌ی نودی‌ هاشو فراخوانده
+        سلام‌فرمانده
+        
+        ⊶⊰✾⊱⊷⊶⊰✾⊱⊷⊶⊰✾⊱⊷
+        
+        نبین قدم کوچیکه پاش بیفته من برات قیام میکنم
+        نبین قدم کوچیکه مثل میرزا کوچک کارو تموم میکنم
+        نبین قدم کوچیکه از صف 313 تا بهت سلام میکنم
+        ⊶⊰✾⊱⊷⊶⊰✾⊱⊷⊶⊰✾⊱⊷
+        نـبین کمه سنم تو فقط صدام بزن ببین چیکار برات میکنم
+        نبین کمه سنم با همین دستای کوچیکم همش دعات میکنم
+        نبین کمه سنم بأبی انت و أمی همه رو فدات میکنم
+        همه رو فدات میکنم
+        ⊶⊰✾⊱⊷⊶⊰✾⊱⊷⊶⊰✾⊱⊷
+        سلام فرمانده سلام از این نسل غیور جامانده
+        سـلام فرمانده سیدعلی دهه‌ی نودی‌ هاشو فراخوانده
+        سلام فرمانده
+        ⊶⊰✾⊱⊷⊶⊰✾⊱⊷⊶⊰✾⊱⊷
+        عهد میبندم روزی لازمت بشم عهد میبندم حاج قاسمت بشم
+        عهد میبندم مثل بهجت و مثل سربازای گمنام خادمت بشم
+        عهد میبندم که میمونم پای کار این نظام
+        کاشکی مثل حاج قاسم من به چشم تو بیام
+        من به چشم تو بیام
+        ⊶⊰✾⊱⊷⊶⊰✾⊱⊷⊶⊰✾⊱⊷
+        هزارو صد و اندی ساله همه عالم دنبال مهدی اند
+        غصه سربازو نخور آقا سربازات هزارو چهارصدی اند
+        
+        ⊶⊰✾⊱⊷⊶⊰✾⊱⊷⊶⊰✾⊱⊷
+        
+        سلام فرمانده سلام از این نسل غیور جامانده
+        سـلام فرمانده سیدعلی دهه‌ی نودی‌ هاشو فراخوانده
+        سلام‌فرمانده
+        سلام‌فرمانده  ` 
+    },
+    { 
+        src: 'sounds/track3.mp3', 
+        img: 'img/img3.jpg', 
+        title: ' به طاها به یاسین ', 
+        lyrics: `  به طاها به یاسین به معراج احمد
+
+        به قدر و به کوثر به رضوان و طوبی
+        به وحی الهی به قرآن جاری
+        به تورات موسی و انجیل عیسی
+        بسی پادشاهی کنم در گدایی
+        چو باشم گدای گدایان زهرا (س)
+        چه شب ها که زهرا (س) دعا کرده تا ما
+        همه شیعه گردیم و بی تاب مولا
+        غلامی این خانواده دلیل و مراد خدا بوده از خلقت ما
+        مسیرت مشخص، امیرت مشخص، مکن دل ای دل بزن دل به دریا
+        که دنیا به خسران عقبا نیرزد
+        به دوری ز اولاد زهرا نیرزد.
+        و این زندگانی فانی جوانی
+        خوشی های امروز و اینجا
+        به افسوس بسیار فردا نیرزد 
+        
+        اگر عاشقانه هوادار یاری
+        اگر مخلصانه گرفتار یاری
+        اگر آبرو میگذاری به پایش
+        یقینا یقینا خریدار یاری
+        بگو چند جمعه گذشتی ز خوابت؟
+        چه اندازه در ندبه ها زاری یابی؟
+        به شانه کشیدی غم سینه اش را؟
+        و یا چون بقیه تو سربار یاری
+        اگر یک نفر را به او وصل کردی
+        برای سپاهش تو سردار یاری
+        به گریه شبی را سحر کردی یا نه؟
+        چه مقدار بی تاب و بیمار یاری؟
+        دل آشفته بودن دلیل کمی نیست
+        اگر بی قراری بدان یار یاری
+        و پایان این بی قراری بهشت است
+        بهشتی که سرخوش ز دیدار یاری 
+        
+        نسیم کرامت وزیدن گرفته
+        و باران رحمت چکیدن گرفته
+        مبادا بدوزی نگاه دلت را
+        به مردم که بازار یوسف فروشی در این دوره بد شدیدا گرفته
+        خدایا به روی درخشان مهدی
+        به زلف سیاه و پریشان مهدی
+        به قلب رئوفش که دریای داغ است
+        به چشمان از غصه گریان مهدی
+        به لبهای گرم علی یا علی اش
+        به ذکر حسین و حسن جان مهدی
+        به دست کریم و نگاه رحیمش
+        به چشم امید فقیران مهدی
+        به حال نیاز و قنوت نمازش
+        به سبحان سبحان سبحان مهدی
+        به برق نگاه به خال سیاهش
+        به عطر ملیح گریبان مهدی
+        به حج جمیلش به جاه جلیلش
+        به صوت حجازی قرآن مهدی
+        به صبح عراق و شبانگاه شامش
+        به آهنگ سمت خراسان مهدی
+        به جان داده های مسیر عبورش
+        به شهد شهود شهیدان مهدی
+        مرا دائم الاشتیاقش بگردان
+        مرا سینه چاک فراقش بگردان
+        تفضل بفرما بر این بنده بی سر و پا
+        مرا همدم و محرم و هم رکاب
+        سفرهای سوی خراسان و شام و عراقش بگردان
+        یا مهدی یامهدی مددی  ` 
+    },
+    { 
+        src: 'sounds/track4.mp3', 
+        img: 'img/img4.jpg', 
+        title: '  ...ندیدم شهی در ', 
+        lyrics: `  ندیدم شهی در دل آرایی تو
+        به قربان اخلاق مولایی تو
+        
+        تو خورشیدی و ذره پرور ترینی
+        فدای سجایای زهرایی تو
+        
+        نداری به کویت ز من بی نواتر
+        ندیدم کریمی به طاهایی تو
+        
+        نداری گدایی به رسوایی من
+        ندیدم نگاری به زیبایی تو
+        
+        نداری مریضی به بد حالی من
+        ندیدم دمی چون مسیحایی تو
+        
+        نداری غلامی به تنهایی من
+        ندیدم غریبی به تنهایی تو
+        
+        نداری اسیری به شیدایی من
+        ندیدم کسی را به آقایی تو
+        
+        امید غریبان تنها کجایی؟
+        چراغ سر قبر زهرا کجایی؟
+        
+        تجلی طه، گل اشک مولا، دل آشفته ی داغ آن کوچه ی غم
+        
+        گرفتار گودال خولی، گرفتار غم های زینب، سیه پوش قاسم
+        
+        عزادار اکبر گل باغ لیلا، پریشان دست علم گیر سقا
+        
+        نفس های سجاد، نواهای باقر، دعاهای صادق
+        
+        کس بی کسی های شب های کاظم
+        
+        حبیب رضا و انیس غریب جواد الائمه
+        
+        تمنای هادی، عزیز دل عسکری، پس نگارا بفرما کجایی؟
+        
+        دلم جز هوایت هوایی ندارد
+        لبم غیر نامت نوایی ندارد
+        
+        وضو و اذان و نماز و قنوتم
+        بدون ولایت بهایی ندارد
+        
+        دلی که نشد خانه ی یاس نرگس
+        خراب است و ویران صفایی ندارد
+        
+        بیا تا جوانم بده رخ نشانم
+        که این زندگانی وفایی ندارد  `
+    },
+];
+
+let audioIndex = 0;
+
+let audioEl = document.getElementById('audio');
+let audioLength = document.getElementById('audio-length');
+
+let pauseBtn = document.querySelector('.fa-pause');
+let playBtn = document.querySelector('.fa-play');
+
+let faVolumeHigh = document.querySelector('.fa-volume-high');
+let faVolumeXmark = document.querySelector('.fa-volume-xmark');
+
+let prevBtn = document.querySelector('.fa-backward');
+let nextBtn = document.querySelector('.fa-forward');
+let shuffleBtn = document.querySelector('.fa-shuffle');
+
+let faHeartRegular = document.querySelector('.fa-heart-regular');
+let faHeartSolid = document.querySelector('.fa-heart-solid');
+
+let musicCover = document.querySelector('.card-img');
+let lyricsModal = document.getElementById('myModal');
+
+function setMusicCover(index) {
+    musicCover.src = audioFiles[index].img;
+}
+
+function formatTime(time) {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes}:${(seconds < 10 ? '0' : '')}${seconds}`;
+}
+
+function setMusicTitle(index) {
+    document.getElementById('music-title').innerText = audioFiles[index].title;
+}
+
+function updateLyricsModal(index) {
+    document.querySelector('.modal-content p').innerText = audioFiles[index].lyrics;
+}
+
+function setMusicInfo(index) {
+    setMusicCover(index);
+    setMusicTitle(index);
+    updateLyricsModal(index);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    audioEl.addEventListener('loadedmetadata', () => {
+        audioLength.max = 100;
+        audioLength.value = 0;
+        setMusicInfo(audioIndex);
+    });
+
+    audioEl.addEventListener('timeupdate', () => {
+        const currentTime = formatTime(audioEl.currentTime);
+        const duration = formatTime(audioEl.duration);
+        document.getElementById('time-start').innerText = currentTime;
+        document.getElementById('time-end').innerText = duration;
+
+        const percentage = (audioEl.currentTime / audioEl.duration) * 100;
+        audioLength.value = percentage;
+    });
+
+    playBtn.addEventListener('click', () => {
+        playBtn.style.display = 'none';
+        pauseBtn.style.display = 'block';
+
+        if (audioEl.paused) {
+            if (!audioEl.src || audioEl.src === '') {
+                audioEl.src = audioFiles[audioIndex].src;
+                setMusicInfo(audioIndex);
+            }
+            audioEl.play();
+        }
+    });
+
+    pauseBtn.addEventListener('click', () => {
+        playBtn.style.display = 'block';
+        pauseBtn.style.display = 'none';
+        audioEl.pause();
+    });
+
+    prevBtn.addEventListener('click', () => {
+        audioIndex = (audioIndex - 1 + audioFiles.length) % audioFiles.length;
+        audioEl.src = audioFiles[audioIndex].src;
+        setMusicInfo(audioIndex);
+        audioEl.play();
+    });
+
+    nextBtn.addEventListener('click', () => {
+        audioIndex = (audioIndex + 1) % audioFiles.length;
+        audioEl.src = audioFiles[audioIndex].src;
+        setMusicInfo(audioIndex);
+        audioEl.play();
+    });
+
+    shuffleBtn.addEventListener('click', () => {
+        shuffle(audioFiles);
+        audioIndex = 0;
+        audioEl.src = audioFiles[audioIndex].src;
+        setMusicInfo(audioIndex);
+        audioEl.play();
+    });
+
+    faHeartRegular.addEventListener('click', () => {
+        faHeartRegular.style.display = 'none';
+        faHeartSolid.style.display = 'block';
+    });
+
+    faHeartSolid.addEventListener('click', () => {
+        faHeartRegular.style.display = 'block';
+        faHeartSolid.style.display = 'none';
+    });
+
+    faVolumeHigh.addEventListener('click', () => {
+        faVolumeHigh.style.display = 'none';
+        faVolumeXmark.style.display = 'block';
+        audioEl.muted = true;
+    });
+
+    faVolumeXmark.addEventListener('click', () => {
+        faVolumeHigh.style.display = 'block';
+        faVolumeXmark.style.display = 'none';
+        audioEl.muted = false;
+    });
+});
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+    lyricsModal.style.top = 0;
+}
+
+span.onclick = function () {
+    lyricsModal.style.top = "-1100px";
+}
+
+window.onclick = function (event) {
+    if (event.target == lyricsModal) {
+        lyricsModal.style.top = "-1100px";
+    }
+}
